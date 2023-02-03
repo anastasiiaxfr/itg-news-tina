3024eb049fbef21bbcf7845db417995efde18871
Cornelius2017 <xforealn@gmail.com>
2023-02-03 09:33:36 +0200

    fix

--- .tina/config.js
+++ .tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "0937f1ed-95da-43d5-b16a-8146f7ef1439", // Get this from tina.io
-  token: "36ef201c935b20ee9786fbab3c4e89344ad9dde5", // Get this from tina.io
+  clientId: process.env.CLIENT_ID, // Get this from tina.io
+  token: process.env.TOKEN, // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
